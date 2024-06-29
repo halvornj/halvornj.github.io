2410fc661b5933bbaebe8060b584d6cf10d6d67b
halvornj <halvor@njastad.no>
2024-06-29 23:56:51 +0200

    did not work on remote

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,10 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+    "title": "About",
+    "excerpt":"This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for Minima at GitHub: jekyll / minima You can find the source code for Jekyll at GitHub: jekyll...","url": "http://localhost:4000/about/"
+  }]
