f76190ae97fe9bb7f8a76e71fe2c2557d43408f3
halvornj <halvor@njastad.no>
2024-07-06 19:20:36 +0200

    added basic homepage, a tags page and rewrote projects

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,34 +1,31 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/welcome-to-jekyll/",
-        "teaser": null
-      },{
         "title": "Olsalget",
         "excerpt":"olalget.no - the primary function This site is available at olsalget.no, and the source code is available (under the apache-2.0 license) here. It was made in collaboration with Ruben Vega Bjørkøy A centralized, easy to use, website for viewing sales times of alcoholic beverages under 4,5% in Norway. There were...","categories": [],
-        "tags": [],
-        "url": "/projects/2023-12-23-olsalget/",
+        "tags": ["Application-Programming-Interfaces","JavaScript","Git","Collaborative-Problem-Solving","Creative-Problem-Solving","Web-Development"],
+        "url": "/olsalget/",
         "teaser": null
       },{
         "title": "værbuddy",
-        "excerpt":"værbuddy is the result of a 3 month software development project, made by 6 students as a project for a university course. Source code and further documentation here IN2000 IN2000 is a course at the University of Oslo. This course is mandatory for students studying computer science (such as me!),...","categories": [],
-        "tags": [],
-        "url": "/projects/V%C3%A6rBuddy/",
+        "excerpt":"værbuddy is the result of a 3 month software development project, made by 6 students as a project for a university course. Source code and further documentation here IN2000 What is IN2000 IN2000 is a course at the University of Oslo. It’s full title is actually Software Engineering with Project....","categories": [],
+        "tags": ["Kotlin","Agile","Android","Sofware-engineering"],
+        "url": "/V%C3%A6rBuddy/",
         "teaser": null
       },{
         "title": "Portfolio site",
         "excerpt":"work in progress, i guess  ","categories": [],
         "tags": ["Web-Development","jekyll"],
-        "url": "/projects/2024-06-30-portfolio-site/",
+        "url": "/portfolio-site/",
         "teaser": null
-      },{
+      },,{
     "title": "Contact",
     "excerpt":"   phone: (+47)916 10 215   mail: halvor@njastad.no   LinkedIn: LinkedIn.com/in/halvornj   github: github.com/halvornj  ","url": "http://localhost:4000/contact/"
   },{
     "title": "Halvor Njåstad",
-    "excerpt":"About  ","url": "http://localhost:4000/"
+    "excerpt":"About me I am a 20 year old computer science student, currently doing the final year of my bachelors degree in Programming and systems architecture. I’m passionate about programming, and am constantly trying to learn as much as possible. This is what’s spawned quite a few personal projects I’m doing...","url": "http://localhost:4000/"
   },{
     "title": "Projects",
     "excerpt":" ","url": "http://localhost:4000/projects/"
+  },{
+    "title": "Posts by Tag",
+    "excerpt":" ","url": "http://localhost:4000/tags/"
   }]
--- index.md
+++ index.md
@@ -6,4 +6,24 @@ layout: single
 author_profile: true
 ---
 
-### About
+## About me
+
+I am a {{site.time|date: "%Y" | minus:2004}} year old computer science student, currently doing the final year of my bachelors degree in **Programming and systems architecture**. I'm passionate about programming, and am constantly trying to learn as much as possible. This is what's spawned quite a few personal projects I'm doing alongside my degree, such as this one.
+
+but, WHOAMI?
+I'll concede, this might not be the only portfolio / blog-site using jekyll hosted on github pages by a CS student, so let me paint a better picture of me:
+Let's see. A couple of other things I'm also really passionate about are music and fitness. In fact, if I were to try and rank the domains in which I feel I have the most knowledge, first, second and third would be Computer Science, music and fitness. So, a lot of my free time is spent either doing strength training, bouldering or playing electric bass.
+
+## Education
+
+#### The university of Oslo: Institute of computer science
+
+Bachelors degree in "Programming and Systems Architecture". | 2022-current
+
+#### International School Zurich North
+
+International A-levels in physics, chemistry, pure mathematics and statistics. | 2021
+
+#### Trondheim Cathedral School
+
+High school, qualifications for higher education, science focused. | 2019 - 2022
